2b3620cfa271f7cc176c4f0ed3cf7f18724f88d3
herbertUG <herbertoketayot@gmail.com>
2025-01-05 16:57:56 +0300

    connect to db, prisma schema

--- actions/login.ts
+++ actions/login.ts
@@ -1,22 +1,44 @@
 "use server";
 
-import { LoginSchema, RegisterSchema } from '@/app/schemas';
-import * as z from 'zod';
+import { LoginSchema, RegisterSchema } from "@/app/schemas";
+import { getUserByEmail } from "@/data/user";
+import { db } from "@/lib/db";
+import bcrypt from "bcrypt";
+import * as z from "zod";
 
-export const login = async (values:z.infer<typeof LoginSchema>) => {
-    const validatedFields = LoginSchema.safeParse(values);
-    if(!validatedFields.success) {
-        return {error:"Invalid fields"};
-    }
+export const login = async (values: z.infer<typeof LoginSchema>) => {
+  const validatedFields = LoginSchema.safeParse(values);
+  if (!validatedFields.success) {
+    return { error: "Invalid fields" };
+  }
 
-    return {success:"Email sent"};
-}
+  return { success: "Email sent" };
+};
 
-export const register = async (values:z.infer<typeof RegisterSchema>) => {
-    const validatedFields = RegisterSchema.safeParse(values);
-    if(!validatedFields.success) {
-        return {error:"Invalid fields"};
-    }
+export const register = async (values: z.infer<typeof RegisterSchema>) => {
+  const validatedFields = RegisterSchema.safeParse(values);
+  if (!validatedFields.success) {
+    return { error: "Invalid fields" };
+  }
 
-    return {success:"Email sent"};
-}
+  const { name, email, password } = validatedFields.data;
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const exisitingUser = await getUserByEmail(email);
+
+  if (exisitingUser) {
+    return { error: "User already exists" };
+  }
+
+  await db.user.create({
+    data: {
+      name,
+      email,
+      password: hashedPassword,
+    },
+  });
+
+//   todo: send email
+
+  return { success: "User created" };
+};
