bafdea0651afa9cc03fe04d6498958aaf0cb5da8
herbertUG <herbertoketayot@gmail.com>
2025-01-05 04:46:02 +0300

    main page UI implementataion

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,21 @@
+import { LoginButton } from "@/components/auth/login-button";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
    <main className="flex h-full flex-col items-center justify-center bg-sky-500">
-    <div className="space-y-6">
+    <div className="space-y-6 text-center">
       <h1 className="text-6xl font-semibold text-center text-white drop-shadow-md">
         Auth
       </h1>
+      <p className="text-white text-lg">
+        A simple authentication service
+      </p>
+      <div>
+        <LoginButton>
+        <Button variant="secondary" size="lg">Sign in</Button>
+        </LoginButton>
+      </div>
     </div>
    </main>
   );
